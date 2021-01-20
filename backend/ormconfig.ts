fa427fd9a1cdecfe8085c9c42c8495ef93ffd892
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-01-20 20:43:20 +0530

    fix ormconfig

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,14 +1,26 @@
 require('dotenv').config();
 import config from './config';
 import { ConnectionOptions } from 'typeorm';
-import User from './entity/user';
+let entities = [];
+if ((require as any).context != null) {
+  const entityContext = (require as any).context('../../wakanda/backend/entity/', true, /\.ts$/);
+  entities = entityContext
+    .keys()
+    .map((modulePath) => entityContext(modulePath))
+    .reduce((result, migrationModule) => {
+      result.concat(
+        Object.keys(migrationModule).map((key) => migrationModule[key]),
+        [],
+      );
+    });
+}
 
 export default {
   ...config.DB,
   type: 'postgres',
   synchronize: false,
   logging: false,
-  entities: [User],
+  entities: Object.values(entities),
   migrations: ['migration/**/*.ts'],
   subscribers: ['subscriber/**/*.ts'],
   cli: {
