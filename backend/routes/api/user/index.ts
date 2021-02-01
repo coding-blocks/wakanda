329c1b7145e7c9c6456af5f2a6d6e65e6f253ee3
Jatin Katyal <jatin.katyal13@gmail.com>
2021-02-01 23:44:33 +0530

    add: error handling in controller handlers

--- backend/routes/api/user/index.ts
+++ backend/routes/api/user/index.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from 'express';
+import { ce } from '../../../utils/app';
 import { isAuthenticated } from '../../../middlewares/authentication';
 import controller from './controller';
 
 const router = Router();
 
-router.get('/me', isAuthenticated, controller.handleGetMe);
+router.get('/me', isAuthenticated, ce(controller.handleGetMe));
 
 export default router;
