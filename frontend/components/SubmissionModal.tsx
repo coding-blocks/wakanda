34af1fad81f5264a71aafa1a9c962e9b224210e2
1arp <dekhijaegi@gmail.com>
2021-02-02 22:52:17 +0530

    reflect changes on submission

--- frontend/components/SubmissionModal.tsx
+++ frontend/components/SubmissionModal.tsx
@@ -40,6 +40,7 @@ export const SubmissionModal: React.FC<any> = (props) => {
           submission,
         }),
       );
+      window.location.reload();
       return submitForReviewRequest;
     }
 
@@ -50,6 +51,7 @@ export const SubmissionModal: React.FC<any> = (props) => {
         submission,
       }),
     );
+    window.location.reload();
     return createSubmissionRequest;
   };
 
