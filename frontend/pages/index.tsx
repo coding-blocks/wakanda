caeb7d8c154bf5a2ac83bbb51b59b659095d4772
BipinKalra <bipinkalra@gmail.com>
2021-02-16 00:27:41 +0530

    footer

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -116,6 +116,176 @@ export const RedirectToLogin: React.FC = () => {
           </div>
         </div>
       </div>
+
+      <div className="landing-page__section landing-page__section--footer">
+        <div className="width-limiter">
+          <div className="row no-gutters pb-50" style={{ borderBottom: '1px solid #cccccc' }}>
+            <div className="col-lg-9 col-12 pr-lg-20 pb-lg-none pb-50 landing-page__section--footer__main-section">
+              <div className="row no-gutters">
+                <div className="col-sm-3 col-6 h-inherit pr-xl-50 pr-lg-30 pr-20">
+                  <div className="flex-col justify-content-between h-100">
+                    <div className="font-4 bold w-100">PRODUCTS</div>
+                    <div className="w-100 mt-50">
+                      <div className="mb-30">
+                        <a href="https://online.codingblocks.com/" className="font-4">
+                          Online Coding Blocks
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://hack.codingblocks.com/" className="font-4">
+                          Hacker Blocks
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://hire.codingblocks.com/" className="font-4">
+                          Hiring Blocks
+                        </a>
+                      </div>
+                      <div>
+                        <a href="https://ide.codingblocks.com/" className="font-4">
+                          Online IDE
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-3 col-6 h-inherit pr-xl-50 pr-lg-30 pr-20">
+                  <div className="flex-col justify-content-between h-100">
+                    <div className="font-4 bold w-100">COMPANY</div>
+                    <div className="w-100 mt-50">
+                      <div className="mb-30">
+                        <a href="https://codingblocks.com/about.html" className="font-4">
+                          About Us
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://codingblocks.com/team.html" className="font-4">
+                          Meet the Team
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://codingblocks.com/success-stories.html" className="font-4">
+                          Success Stories
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://codingblocks.com/signup/job.html" className="font-4">
+                          Become a TA/Mentor
+                        </a>
+                      </div>
+                      <div>
+                        <a href="https://blog.codingblocks.com/" className="font-4">
+                          Blog
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-3 col-6 h-inherit pr-xl-50 pr-lg-30 pr-20 mt-sm-none mt-50">
+                  <div className="flex-col justify-content-between h-100">
+                    <div className="font-4 bold w-100">RESOURCES</div>
+                    <div className="w-100 mt-50">
+                      <div className="mb-30">
+                        <a
+                          href="https://www.youtube.com/channel/UCICWIYEx2mo4wYZzLwJ7wVw"
+                          className="font-4"
+                        >
+                          Watch Youtube Videos
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://blog.codingblocks.com/" className="font-4">
+                          Blogs
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="#" className="font-4">
+                          Telegram Channel
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="#" className="font-4">
+                          Study Material
+                        </a>
+                      </div>
+                      <div>
+                        <a href="#" className="font-4">
+                          Career Advice
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-3 col-6 h-inherit pr-xl-50 pr-lg-30 pr-20 mt-sm-none mt-50">
+                  <div className="flex-col justify-content-between h-100">
+                    <div className="font-4 bold w-100">COMMUNITY</div>
+                    <div className="w-100 mt-50">
+                      <div className="mb-30">
+                        <a
+                          href="https://codingblocks.com/campus-ambassador-program.html"
+                          className="font-4"
+                        >
+                          Campus Ambassador
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="https://codingblocks.com/events" className="font-4">
+                          Events and Workshops
+                        </a>
+                      </div>
+                      <div className="mb-30">
+                        <a href="#" className="font-4">
+                          BOSS 2021
+                        </a>
+                      </div>
+                      <div>
+                        <a href="https://ide.codingblocks.com/" className="font-4">
+                          Online IDE
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-12 mt-lg-none mt-50 pl-xl-70 pl-lg-50">
+              <div className="row no-gutters">
+                <div className="col-lg-12 col-sm-6 col-12 mb-lg-50">
+                  <div className="bold font-4 mb-sm-40 mb-30">CONTACT</div>
+                  <div className="font-4 mb-lg-30 mb-sm-20 mb-15">admissionscodingblocks.com</div>
+                  <div className="font-4">1800 274 4504</div>
+                </div>
+                <div className="col-lg-12 col-sm-6 col-12 mt-sm-none mt-50">
+                  <div className="bold font-4 mb-sm-40 mb-30">TECHNICAL SUPPORT</div>
+                  <div className="font-4 mb-lg-30 mb-sm-20 mb-15">supportcodingblocks.com</div>
+                  <div className="font-4">+91 964 333 9747</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row no-gutters align-items-center justify-content-sm-between justify-content-center mt-50">
+            <img
+              src="https://codingblocks.com/assets/images/cb/cblogo.png"
+              style={{ height: '85px' }}
+            />
+            <div className="flex-1 t-align-xl-c t-align-r pl-xl-none pl-20 d-sm-block d-none">
+              <div className="font-4 mb-10">Privacy Policy | Terms of Service</div>
+              <div className="font-4">Copyright 2020 - Coding Blocks Pvt. Ltd.</div>
+            </div>
+            <div className="col-12 t-align-c d-sm-none d-block mt-50">
+              <div className="font-4 mb-10">Privacy Policy | Terms of Service</div>
+              <div className="font-4">Copyright 2020 - Coding Blocks Pvt. Ltd.</div>
+            </div>
+            <img
+              src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/socials.svg"
+              className="d-xl-block d-none"
+            />
+          </div>
+          <div className="mt-50 d-xl-none d-block t-align-c">
+            <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/socials.svg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
